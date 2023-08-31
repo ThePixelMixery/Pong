5c78c6094e21355c8fee4f629dc58594ed2f5558
ThePixelMixery <83445650+ThePixelMixery@users.noreply.github.com>
2023-09-01 07:14:47 +1000

    Music Controls Done

--- Assets/Scripts/AudioControl.cs
+++ Assets/Scripts/AudioControl.cs
@@ -5,10 +5,21 @@ using UnityEngine.Audio;
 
 public class AudioControl : MonoBehaviour
 {
-    public AudioMixer mixer_FX;
+    public AudioMixer mixer_master;
 
-    public void SetLevel(float sliderValue)
+    public void SetMasterLevel(float sliderValue)
     {
-        mixer_FX.SetFloat("FX_Volume", Mathf.Log10(sliderValue) * 20);
+        mixer_master.SetFloat("masterVolume", Mathf.Log10(sliderValue) * 20);
     }
+
+    public void SetMusicLevel(float sliderValue)
+    {
+        mixer_master.SetFloat("musicVolume", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetFxLevel(float sliderValue)
+    {
+        mixer_master.SetFloat("fxVolume", Mathf.Log10(sliderValue) * 20);
+    }
+    
 }
